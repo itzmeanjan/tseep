ea051861192253b3de45c263491b360d89950c0c
Anjan Roy <anjanroy@yandex.com>
2021-06-02 18:05:39 +0530

    opcode read/ write method; read envelope

--- op/ops.go
+++ op/ops.go
@@ -1,8 +1,47 @@
 package op
 
+import (
+	"encoding/binary"
+	"io"
+)
+
 type OP uint8
 
 const (
 	READ OP = iota + 1
 	WRITE
 )
+
+func (o OP) WriteTo(w io.Writer) (int64, error) {
+	if err := binary.Write(w, binary.BigEndian, o); err != nil {
+		return 0, err
+	}
+
+	return 1, nil
+}
+
+func (o *OP) ReadFrom(r io.Reader) (int64, error) {
+	var op uint8
+
+	if err := binary.Read(r, binary.BigEndian, &op); err != nil {
+		return 0, err
+	}
+
+	*o = OP(op)
+	return 1, nil
+}
+
+func ReadEnvelope(r io.Reader) (OP, uint32, error) {
+	op := new(OP)
+
+	if _, err := op.ReadFrom(r); err != nil {
+		return 0, 0, err
+	}
+
+	var bodyLength uint32
+	if err := binary.Read(r, binary.BigEndian, &bodyLength); err != nil {
+		return *op, 0, err
+	}
+
+	return *op, bodyLength, nil
+}
