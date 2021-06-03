82e2a8bd5a9ece1031927dd798dccd51150d7ab5
Anjan Roy <anjanroy@yandex.com>
2021-06-03 18:17:53 +0530

    server v1 testing

---
+++ v1/server_test.go
@@ -0,0 +1,52 @@
+package v1_test
+
+import (
+	"bytes"
+	"context"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/itzmeanjan/tseep/op"
+	v1 "github.com/itzmeanjan/tseep/v1"
+)
+
+func TestServerV1(t *testing.T) {
+	proto := "tcp"
+	addr := "127.0.0.1:13000"
+	ctx, cancel := context.WithCancel(context.Background())
+	server, err := v1.New(ctx, proto, addr)
+	if err != nil {
+		t.Fatalf("Failed to start TCP server : %s\n", err.Error())
+	}
+
+	d := net.Dialer{
+		Timeout:  10 * time.Second,
+		Deadline: time.Now().Add(20 * time.Second),
+	}
+	conn, err := d.DialContext(ctx, proto, server.Addr)
+	if err != nil {
+		t.Fatalf("Failed to dial TCP server : %s\n", err.Error())
+	}
+
+	key := op.Key("hello")
+	rReq := op.ReadRequest{Key: &key}
+	if _, err := rReq.WriteEnvelope(conn); err != nil {
+		t.Fatalf("Failed to write request envelope : %s\n", err.Error())
+	}
+
+	if _, err := rReq.WriteTo(conn); err != nil {
+		t.Fatalf("Failed to write read request body : %s\n", err.Error())
+	}
+
+	resp := new(op.Value)
+	if _, err := resp.ReadFrom(conn); err != nil {
+		t.Fatalf("Failed to read response : %s\n", err.Error())
+	}
+
+	if !bytes.Equal(*resp, []byte("")) {
+		t.Fatalf("Expected to receive empty response\n")
+	}
+
+	cancel()
+}
