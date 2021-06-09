baf56075319a1f7b48cb943dffe1f73212b15988
Anjan Roy <anjanroy@yandex.com>
2021-06-09 18:13:12 +0530

    multi client

--- utils/env.go
+++ utils/env.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"os"
 	"strconv"
+	"time"
 )
 
 func GetAddr() string {
@@ -21,4 +22,24 @@ func GetPort() uint64 {
 	}
 
 	return 7000
+}
+
+func GetClientCount() uint64 {
+	if client, ok := os.LookupEnv("CLIENT_COUNT"); ok {
+		if parsed, err := strconv.ParseUint(client, 10, 64); err == nil {
+			return parsed
+		}
+	}
+
+	return 1
+}
+
+func GetDelay() time.Duration {
+	if delay, ok := os.LookupEnv("DELAY"); ok {
+		if parsed, err := time.ParseDuration(delay); err == nil {
+			return parsed
+		}
+	}
+
+	return time.Second
 }
---
+++ v1/client/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/itzmeanjan/tseep/op"
+	"github.com/itzmeanjan/tseep/utils"
+)
+
+func main() {
+	clientCount := utils.GetClientCount()
+	clients := make([]net.Conn, 0, clientCount)
+	proto := "tcp"
+	addr := fmt.Sprintf("%s:%d", utils.GetAddr(), utils.GetPort())
+	delay := utils.GetDelay()
+
+	for i := 0; i < int(clientCount); i++ {
+		conn, err := net.Dial(proto, addr)
+		if err != nil {
+			log.Printf("Failed to dial TCP server : %s\n", err.Error())
+			continue
+		}
+		defer func() {
+			conn.Close()
+		}()
+
+		clients = append(clients, conn)
+	}
+
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, syscall.SIGTERM, syscall.SIGINT)
+
+	for {
+		select {
+		case <-interruptChan:
+			return
+
+		default:
+			if !read_write(clients, interruptChan) {
+				return
+			}
+
+			<-time.After(delay)
+		}
+	}
+}
+
+func read_write(clients []net.Conn, stopChan chan os.Signal) bool {
+OUTER:
+	for _, conn := range clients {
+		select {
+		case <-stopChan:
+			return false
+
+		default:
+			r := rand.Intn(1 << 30)
+
+			switch r%2 == 0 {
+			case true:
+				key := op.Key(fmt.Sprintf("%255d", r))
+				req := op.ReadRequest{Key: &key}
+				if _, err := req.WriteEnvelope(conn); err != nil {
+					log.Printf("Failed to write request envelope : %s\n", err.Error())
+					continue OUTER
+				}
+
+				if _, err := req.WriteTo(conn); err != nil {
+					log.Printf("Failed to write request body : %s\n", err.Error())
+					continue OUTER
+				}
+
+				resp := new(op.Value)
+				if _, err := resp.ReadFrom(conn); err != nil {
+					log.Printf("Failed to read response : %s\n", err.Error())
+					continue OUTER
+				}
+
+				log.Printf("Read %s => %s [%s]\n", key, *resp, conn.LocalAddr())
+
+			case false:
+				key := op.Key(fmt.Sprintf("%255d", r))
+				val := op.Value(fmt.Sprintf("%255d", r))
+				req := op.WriteRequest{Key: &key, Value: &val}
+				if _, err := req.WriteEnvelope(conn); err != nil {
+					log.Printf("Failed to write request envelope : %s\n", err.Error())
+					continue OUTER
+				}
+
+				if _, err := req.WriteTo(conn); err != nil {
+					log.Printf("Failed to write request body : %s\n", err.Error())
+					continue OUTER
+				}
+
+				resp := new(op.Value)
+				if _, err := resp.ReadFrom(conn); err != nil {
+					log.Printf("Failed to read response : %s\n", err.Error())
+					continue OUTER
+				}
+
+				log.Printf("Wrote %s => %s [%s]\n", key, *resp, conn.LocalAddr())
+
+			}
+		}
+	}
+
+	return true
+}
